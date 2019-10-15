10e64c209c1f8a1cae2eeb875bf50d4ca29987fd
0774775 <0774775@KELCE-LABS17-P.pittstate.edu>
2019-10-15 11:32:58 -0500

    Lab4 part 2

--- src/main/java/TestCar.java
+++ src/main/java/TestCar.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class TestCar {
 
     public static void main(String[] args) throws IOException{
-
+ 
         Car car = new Car(2019, "Mustang");
    
         System.out.println("Starting Car Speed : " + car.getCarSpeed());
