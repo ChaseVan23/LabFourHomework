37f064601529c50fdd6010b84687e3e96b23d202
0774775 <0774775@KELCE-LABS17-P.pittstate.edu>
2019-10-15 11:10:48 -0500

    Lab4 part 2

---
+++ src/main/java/Car.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 0774775
+ */
+public class Car {
+    
+    private int yearModel;
+    private String make;
+    private int speed;
+
+public Car(int yearModel, String make) {
+
+    this.yearModel = yearModel;
+    this.make = make;
+    this.speed = 0;
+
+}
+
+public void accelerate() {
+
+this.speed = this.speed + 5;
+
+System.out.println("Current Car Speed: " + this.speed);
+
+}
+
+public void brake() {
+
+this.speed = this.speed - 5;
+
+System.out.println("Current Car Speed : " + this.speed);
+
+}
+
+public int getCarSpeed() {
+
+    return this.speed;
+  }
+
+}
